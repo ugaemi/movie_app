38752d94eb250255c9485a646919e71847c09913
ugaemi <u.gaemi@gmail.com>
2021-07-25 23:24:46 +0900

    Lecture #3.1 ~ #4.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: [],
   };
-  add = () => {
-    console.log("add");
-  };
-  minus = () => {
-    console.log("minus");
-  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false})
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
   render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const {isLoading, movies} = this.state;
+    return <div>{isLoading ? "Loading..." : movies.map(movie => (
+      <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title}
+             summary={movie.summary} poster={movie.medium_cover_image}/>
+    ))}</div>;
   }
 }
 
